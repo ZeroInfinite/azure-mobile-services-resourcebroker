bb3ceea51c0bffc6fd3c51d67480f7718040908a
Jason Allor <jasonall@microsoft.com>
2014-08-22 13:23:00 -0700

    Update nuget generation script

--- runtime/net/Microsoft.WindowsAzure.Mobile.Service.ResourceBroker/GenerateNuget.cmd
+++ runtime/net/Microsoft.WindowsAzure.Mobile.Service.ResourceBroker/GenerateNuget.cmd
@@ -1,1 +1,8 @@
-..\..\..\tools\Nuget.exe pack Microsoft.WindowsAzure.Mobile.Service.ResourceBroker.csproj -Prop Configuration=Release
+@echo off
+if not exist bin\Nugets md bin\Nugets
+
+echo .
+echo Nuget will be placed in the bin\Nugets folder
+echo .
+
+..\..\..\tools\Nuget.exe pack Microsoft.WindowsAzure.Mobile.Service.ResourceBroker.csproj -Prop Configuration=Release -OutputDirectory bin\Nugets
